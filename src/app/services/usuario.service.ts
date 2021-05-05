76c8797038a8de3429765736ec2e958d2b7a7095
Pedro Muñoz Burgos <pedroanibal_jr@hotmail.com>
2021-05-05 10:29:24 -0500

    Fin sección 15

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -11,6 +11,8 @@ import { environment } from '../../environments/environment.prod';
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { loginForm } from '../interfaces/login-form.interface';
 
+import { Usuario } from '../models/usuario.model';
+
 
 
 const base_url = environment.base_url;
@@ -22,6 +24,7 @@ declare const gapi: any;
 export class UsuarioService {
 
   public auth2: any;
+  public usuario: Usuario;
 
   constructor( private http: HttpClient,
                 private router: Router,
@@ -29,6 +32,16 @@ export class UsuarioService {
 
      ) {
        this.googleInit();
+     }
+
+     get token(): string {
+       return localStorage.getItem( 'token' ) || '';
+     }
+
+     get uid(): string{
+
+      return this.usuario.uid || '';
+
      }
 
 
@@ -72,18 +85,24 @@ export class UsuarioService {
 
     validarToken(): Observable<boolean> {
 
-      const token = localStorage.getItem('token') || '';
+
 
       return this.http.get( `${ base_url }/login/renew` ,{
 
         headers: {
-          'x-token': token
+          'x-token': this.token
         }
       }).pipe(
-        tap( ( resp: any ) => {
+        map( ( resp: any ) => {
+          console.log(resp);
+
+          const { email, google, nombre, role, img, uid } = resp.usuario;
+
+
+          this.usuario = new Usuario( nombre, email, '', img, google, role, uid );
           localStorage.setItem('token', resp.token);
+          return true;
         } ),
-        map( resp => true ),
         catchError( error => of( false ) )
       )
 
@@ -99,6 +118,23 @@ export class UsuarioService {
                         localStorage.setItem('token', resp.token)
                       } )
                     )
+  }
+
+
+  actualizarPerfil( data: { email: string, nombre: string, role: string } ){
+
+    data = {
+      ...data,
+      role: this.usuario.role
+    };
+
+    return this.http.put(`${ base_url }/usuarios/${ this.uid }`, data, {
+
+      headers: {
+        'x-token': this.token
+      }
+    });
+
   }
 
 
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.model';
 import { SidebarService } from '../../services/sidebar.service';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,9 +12,14 @@ import { SidebarService } from '../../services/sidebar.service';
 export class SidebarComponent implements OnInit {
 
   menuItems: any[];
+  //public imgUrl = '';
+  public usuario: Usuario;
 
-  constructor( private sidebarService: SidebarService ) {
+  constructor( private sidebarService: SidebarService,
+    private usuarioService: UsuarioService  ) {
     this.menuItems = sidebarService.menu;
+    //this.imgUrl = usuarioService.usuario.imagenUrl;
+    this.usuario = usuarioService.usuario;
   }
 
   ngOnInit(): void {
